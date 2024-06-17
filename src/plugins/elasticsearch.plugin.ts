3aa9bb1a252bef57101b35559ab9cc8703320201
Eyal Levi <happyeyal@gmail.com>
2024-06-17 23:31:07 +0300

    chore: Register Elasticsearch plugin with Fastify server

--- src/plugins/elasticsearch.plugin.ts
+++ src/plugins/elasticsearch.plugin.ts
@@ -4,6 +4,11 @@ import { fastifyElasticsearch } from "@fastify/elasticsearch";
 
 const ELASTICSEARCH_URL = process.env.ELASTICSEARCH_URL || "https://localhost:9200";
 
+/**
+ * Registers the Elasticsearch plugin with the Fastify server.
+ * 
+ * @param server - The Fastify server instance.
+ */
 export const elasticSearchPlugin: FastifyPluginAsync = fp(async (server) => {
   await server.register(fastifyElasticsearch, {
     node: ELASTICSEARCH_URL,
